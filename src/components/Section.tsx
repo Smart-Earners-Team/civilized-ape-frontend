dd7b498fd98c5931186df64e007fdc55c127a48a
Ttebify <ttebify@gmail.com>
2022-08-08 20:31:22 +0100

    Initial commit

---
+++ src/components/Section.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import cls from "classnames";
+
+export default function Section({
+  padding = true,
+  ...props
+}: {
+  children: React.ReactNode;
+  className?: string;
+  containerClass?: string;
+  padding?: boolean;
+}) {
+  const pClass = padding ? "py-20" : "py-10 py-0";
+  return (
+    <div className={cls(props.containerClass)}>
+      <div
+        className={cls(
+          "px-4 md:px-16 max-w-screen-xl mx-auto",
+          pClass,
+          props.className
+        )}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,167 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+import ReactPlayer from "react-player";
+import heroVideo from "../assets/hero_video.mp4";
+import { RiArrowDropRightFill, RiSearchLine } from "react-icons/ri";
+import TypistLoop from "../components/Typist";
+import Typist from "react-typist";
+import Section from "../components/Section";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main>
+      <section className="w-full h-[90vh] hero_video relative">
+        <ReactPlayer
+          url={heroVideo}
+          playing
+          loop
+          muted
+          width="100%"
+          height="100%"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center">
+          <h1 className="text-7xl font-black text-center max-w-6xl text-[#FEF3C0] text-stroke">
+            <TypistLoop interval={0}>
+              {[
+                "Deep in the heart of Brocéliande Forest roam a troop of 10,000 Ape",
+              ].map((text) => (
+                <Typist key={text} startDelay={0}>
+                  {text}
+                  <Typist.Backspace count={text.length} delay={2000} />
+                </Typist>
+              ))}
+            </TypistLoop>
+          </h1>
+        </div>
+      </section>
+      <Section
+        padding
+        className="flex flex-col items-start gap-10 md:gap-2 md:text-center md:items-center"
+        containerClass="bg-[#F3F3F3]/90"
+      >
+        <p className="text-2xl">
+          A community of 10,000 Civilized Apes teeming with life.
+        </p>
+        <p className="text-2xl">
+          We symbolize freedom and the possibility of a life truly lived.
+        </p>
+        <button className="bg-[#B35048] flex justify-center items-center py-2 px-4 text-white rounded-md text-lg md:mt-6">
+          Join Waitlist
+          <RiArrowDropRightFill className="text-white" />
+        </button>
+      </Section>
+      <Section
+        padding
+        className="flex flex-col md:flex-row items-start gap-8 md:justify-around"
+        containerClass="bg-white"
+      >
+        <StaticImage
+          src="../images/Group-9.png"
+          alt="Group 9"
+          className="w-full max-w-lg"
+        />
+        <div className="flex flex-col items-start md:gap-2 max-w-lg">
+          <h2>Become a Digital Art Collector And Discover Rare Digital Art</h2>
+          <p className="text-xl">
+            Our genesis collection is comprised of 10,000 individually unique
+            NFTs and is emblazoned with over 200 traits including expression,
+            headwear, posture, clothing, and more
+          </p>
+        </div>
+      </Section>
+      <Section
+        padding
+        className="flex flex-col md:flex-row items-start gap-8 md:justify-around"
+        containerClass="bg-[#F3F3F3]/90"
+      >
+        <div className="flex flex-col items-start gap-2 max-w-lg">
+          <h2>Building An Alpha Web3 community</h2>
+          <StaticImage
+            src="../images/image 3.png"
+            alt="Group 9"
+            className="w-full max-w-lg md:hidden"
+          />
+          <p className="text-xl my-5">
+            We are on a mission to help you be the best you can be, through our
+            in-depth courses and via consultation. Courses including (but are
+            not limited to)
+          </p>
+          <ol className="list-disc list-inside grid grid-cols-1 sm:grid-cols-2 gap-2 font-medium gap-x-5">
+            <li>Technical Analysis</li>
+            <li>Product Management</li>
+            <li>UI/UX Design</li>
+            <li>Lead Generation</li>
+            <li>Website Development</li>
+            <li>Digital Marketing</li>
+            <li>Branding</li>
+            <li>Money Management</li>
+            <li>Content Creation</li>
+            <li>Career Development</li>
+          </ol>
+        </div>
+        <StaticImage
+          src="../images/image 3.png"
+          alt="Group 9"
+          className="w-full max-w-lg hidden md:block"
+        />
+      </Section>
+      <Section
+        padding
+        className="flex flex-col md:flex-row items-start gap-8 md:justify-around"
+        containerClass="bg-white"
+      >
+        <StaticImage
+          src="../images/Group 3.png"
+          alt="Group 3"
+          className="w-full max-w-lg"
+        />
+        <div className="flex flex-col items-start md:gap-2 max-w-lg">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <ol>
+            <li>
+              We are creating unique member experiences and utility by forging
+              partnerships with projects across the physical and web3 world.
+            </li>
+            <li>
+              cApe NFT holders gain access to the Civilized Ape Town Hall where
+              daily Alpha calls, IDO's, Degens, NFT's, Trading Signals, Coupon
+              codes for some exclusive courses and events etc. are shared.
+            </li>
+          </ol>
+        </div>
+      </Section>
+      <Section
+        padding
+        className="flex flex-col items-start gap-8"
+        containerClass="bg-white"
+      >
+        <div className="flex flex-col md:items-center items-start md:gap-2 bg-[#ECD980] py-14 px-10 md:text-center rounded-3xl">
+          <h2 className="md:text-4xl text-[#4D3937] font-semibold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h2>
+          <p className="font-normal">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel,
+            rhoncus lectus ultricies ut sed imperdiet sit at. In mattis sed
+            fermentum lorem. Cursus mattis ipsum aliquam, sit enim scelerisque
+            ridiculus viverra. Augue sed quam molestie id senectus.
+          </p>
+          <div className="flex justify-between items-stretch my-5 w-full max-w-sm shadow-md">
+            <input
+              type="email"
+              className="bg-white placeholder-gray-600 font-light p-2 w-full"
+              placeholder="Email"
+            />
+            <button className="bg-[#4D3937] flex-none px-2 text-white">
+              <RiSearchLine className="h-6 w-6" />
+            </button>
+          </div>
+        </div>
+      </Section>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
